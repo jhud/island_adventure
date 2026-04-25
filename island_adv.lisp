916fb727c57829014cb7c2c20139db8410e53d61
James Hudson <bugs.feedback.whatever@gmail.com>
2026-04-25 20:45:40 +0200

    IMplemented action system to apply multiple actions per step.

--- island_adv.lisp
+++ island_adv.lisp
@@ -2,8 +2,10 @@
 
 (defun create_world_env ()
   '((time . 0)
-    (player . (10 . 29))
-    (map_size . (32 . 32))))
+    (player_pos . (17 . 52))
+    (map_size . (64 . 64))
+    (message . "")
+    ))
 
 (defun get_val (key alist)
   (cdr (assoc key alist)))
@@ -11,7 +13,6 @@
 (defun set_val (key value alist)
   (cons (cons key value)
         (remove key alist :key #'car :test #'eq)))
-
 
 ;; Get tile attributes at the given coordinates. Lisp does not really need enums, so we use symbols
 (defun tile_attributes (x y world_env)
@@ -31,12 +32,14 @@
 )
 
 (defun pairwise (lst)
+  "Group a list into a list of cons pairs."
   (if lst
       (cons (cons (first lst) (second lst))
             (pairwise (cddr lst)))
       nil))
 
 (defun in_circle (x y cx cy radius)
+  "Is a point inside a circle?"
   (<= (+ (* (- x cx) (- x cx))
          (* (- y cy) (- y cy)))
       (* radius radius)))
@@ -57,39 +60,87 @@
 )
 
 (defun is_tile_water (x y)
-    (not (some (lambda (circle) (in_circle x y (+ 8 (car circle)) (+ 9 (cdr circle)) 9)) (random_2d 9 14 25)))
+  "Procedural function to check if a tile is land or ater on our island."
+  (not (some (lambda (circle) (in_circle x y (+ 15 (car circle)) (+ 15 (cdr circle)) 9)) (random_2d 8 30 15)))
 )
 
-(defun move_player(x_delta y_delta world_env)
-    (let ((player_pos (get_val 'player world_env)))
-        (set_val 'player (cons (+ (car player_pos) x_delta) 
-                               (+ (cdr player_pos) y_delta)) world_env)
-    )
+;; Actions system
+
+(defun run-actions (actions world)
+  "Action pipeline for the world. Apply the functions one by one to world."
+  (if (null actions)
+      world
+      (run-actions (cdr actions)
+                   (funcall (car actions) world))))
+
+(defun add-message (message)
+  "Returns a function which is applied to the world state, which sets the message."
+  (lambda (world_env) (set_val 'message message world_env))
 )
+
+(defun set-player-pos (new-pos)
+  "Returns a function which is applied to the world state, which sets the player pos."
+  (lambda (world_env) (set_val 'player_pos new-pos world_env))
+)
+
+
+(defun move_player (dx dy world_env)
+  "Move a player by the given delta."
+  (let* ((pos (get_val 'player_pos world_env))
+         (x (car pos))
+         (y (cdr pos))
+         (new_pos (cons (+ x dx)
+                        (+ y dy))))
+    (run-actions (list
+      (set-player-pos new_pos)
+      (add-message "You walk a short distance."))
+     world_env)))
+
+(defun lookup (key table)
+  "Look up value in an assoc table"
+  (cdr (assoc key table)
+))
+
+(defun is-tile-navigable (attribute)
+  "Can the player walk on this attribute?"
+  (lookup attribute '(  (forest . t)
+                        (plains . t) 
+                        (empty . t) 
+                        (beach . t) 
+)))
+
+
+(defun move_to_if_free (x-delta y-delta world_env)
+  (let* ((player_pos (get_val 'player_pos world_env)) (try-x (+ (car player_pos) x-delta)) (try-y (+ (cdr player_pos) y-delta)))
+    (if (is-tile-navigable (tile_attributes try-x try-y world_env)) 
+      (move_player x-delta y-delta world_env) 
+      (funcall (add-message "You can't go in that direction.") world_env)))
+  )
+
 
 (defun parse_user_input (user_input world_env)
-    (let ((first_letter (car user_input)))
-        (cond 
-            ((eq first_letter 'w) (move_player 0 -1 world_env)) 
-            ((eq first_letter 's) (move_player 0 1 world_env)) 
-            ((eq first_letter 'a) (move_player -1 0 world_env)) 
-            ((eq first_letter 'd) (move_player 1 0 world_env)) 
-        )
-    )
-)
+  (let ((first-char (char user_input 0)))
+    (cond 
+      ((char-equal first-char #\w) (move_to_if_free 0 -1 world_env)) 
+      ((char-equal first-char #\s) (move_to_if_free 0 1 world_env)) 
+      ((char-equal first-char #\a) (move_to_if_free -1 0 world_env)) 
+      ((char-equal first-char #\d) (move_to_if_free 1 0 world_env))
+      ((char-equal first-char #\h) (print world_env))
+      (t world_env))))
+
 
 (defun render_world (world_env)
     (let ((map_size (get_val 'map_size world_env)))
     (dotimes (y (car map_size))
         (dotimes (x (cdr map_size))
-            (princ (cdr (assoc 
+            (princ (lookup 
                 (tile_attributes x y world_env) '(  (forest . ^)
                                                     (plains . _) 
                                                     (water . w) 
                                                     (empty . e) 
                                                     (beach . b) 
                                                     (cliff . c))
-            ))
+            )
             )
         )
         (terpri)
@@ -97,18 +148,49 @@
     )
 )
 
-(defun main_loop ()
-    (let ((world (create_world_env)))
-        (loop
-            (format t "~&> ")
-            (let ((user_input (read-line)))
-                (when (string-equal user_input "quit")
-                    (return))
-                (parse_user_input user_input world)
+(defun describe_current_location (world_env)
+    (let* ( (player_pos (get_val 'player_pos world_env)) 
+            (tile (tile_attributes (car player_pos) (cdr player_pos) world_env)))
+                (format nil "~{~a~}" 
+                (list 
+                (get_val 'message world_env)
+                (cdr (assoc 
+                    tile '(  (forest . "You are in a forest")
+                            (plains . "You are on an open plain") 
+                            (water . "water (OOB)") 
+                            (empty . "empty") 
+                            (beach . "You are standing on a beach") 
+                            (cliff . "cliff (OOB)"))
+                )) ". "
+                "To the north " (describe_location_brief (cons (car player_pos) (- (cdr player_pos) 1)) world_env) ". "
+                "To the east " (describe_location_brief (cons (+ (car player_pos) 1) (cdr player_pos)) world_env) ". "
+                "To the south " (describe_location_brief (cons (car player_pos) (+ (cdr player_pos) 1)) world_env) ". "
+                "To the west " (describe_location_brief (cons (- (car player_pos) 1) (cdr player_pos)) world_env) ". "
+                )))
             )
-        )
-    )
-)
+
+(defun describe_location_brief (pos world_env)
+    (let ((tile (tile_attributes (car pos) (cdr pos) world_env)))
+            (cdr (assoc 
+                tile '(  (forest . "is a forest")
+                        (plains . "are open plains") 
+                        (water . "are crashing waves") 
+                        (empty . "is empty") 
+                        (beach . "is a beach") 
+                        (cliff . "are sheer cliffs"))
+            ))))
+
+(defun main_loop ()
+  (let ((world (create_world_env)))
+    (loop
+      (format t "~a~%" (describe_current_location world))
+      (format t "~&> ")
+      (finish-output)  ;; ensure prompt is printed immediately
+      (let ((user_input (read-line)))
+        (when (string-equal user_input "quit")
+          (return))
+        (setf world (parse_user_input user_input world)) ;; set world to the new value
+      ))))
 
 
 (render_world (create_world_env))
